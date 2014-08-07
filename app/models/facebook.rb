9a839059d16fbec7f8cf12674048aa26beff1f30
John Kelly <mr.johnkelly@live.com>
2014-08-06 21:32:48 -0700

    Handle facebook token expiration

--- app/models/facebook.rb
+++ app/models/facebook.rb
@@ -7,7 +7,11 @@ class Facebook < AuthProvider
   end
 
   def exchange_access_token(old_access_token)
-    update!(token: oauth.exchange_access_token(old_access_token), expiration: 60.days.from_now)
+    new_token_info = oauth.exchange_access_token_info(old_access_token)
+    new_token = new_token_info.fetch('access_token') do
+      raise 'FB exchange access token api missing token'
+    end
+    update!(token: new_token, expiration: expiration_time(new_token_info))
   end
 
 
@@ -15,5 +19,13 @@ class Facebook < AuthProvider
 
   def oauth
     @oauth ||= Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'])
+  end
+
+  def expiration_time(new_token_info)
+    seconds_to_expiration = new_token_info.fetch('expires') do
+      raise 'FB exchange access token api missing expiration seconds'
+    end
+    days_to_expiration = Integer(seconds_to_expiration) / 1.day
+    days_to_expiration.floor.days.from_now
   end
 end
